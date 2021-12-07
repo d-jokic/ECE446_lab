7f507014aa5d96331659804edd1087a1b1f2d399
Clément Forray <clement.forray@gmail.com>
2021-12-07 18:36:50 -0500

    update the format of the plots

--- pitch_analysis2.m
+++ pitch_analysis2.m
@@ -3,6 +3,10 @@
 % define global variables
 lan = ["english", "french", "german", "serbian"];
 landict = containers.Map(lan,1:4);
+prof = ["full","professional", "working","basic"];
+sizes = [300, 150, 60, 20];
+sizedict =containers.Map(prof,sizes);
+
 sp = soundPitchMethods();
 
 
@@ -21,7 +25,7 @@ N_participants = length(unique_name);
 partdict = containers.Map(unique_name, 1:N_participants);
 
 % init the array to be plotted
-results_pitch = NaN(N_participants, length(lan)+1);
+results_pitch = NaN(N_participants, length(lan)*2);
 
 
 % loop through the table entries
@@ -41,9 +45,8 @@ for i=1:N_entries
     %append to results
     results_pitch(pos,landict(row.LANGUAGE)) = avg_pitch;
 
-    if row.PROFICIENCY == "full"
-        results_pitch(pos,length(lan)+1) = avg_pitch;
-    end
+
+    results_pitch(pos,length(lan)+landict(row.LANGUAGE) ) = sizedict(row.PROFICIENCY);
 
 
 
@@ -53,18 +56,21 @@ end
 %% Plot the pitch results
 
 
-scatter(1:N_participants, results_pitch(:,1), 'filled')
+scatter(1:N_participants, results_pitch(:,1), results_pitch(:,5), 'filled')
+alpha(0.8)
 hold on
-scatter(1:N_participants,results_pitch(:,2), 'filled')
+scatter(1:N_participants,results_pitch(:,2), results_pitch(:,6),'filled')
+alpha(0.8)
 hold on
-scatter(1:N_participants, results_pitch(:,3), 'filled')
+scatter(1:N_participants,results_pitch(:,3), results_pitch(:,7),'filled')
+alpha(0.8)
 hold on
-scatter(1:N_participants, results_pitch(:,4), 'filled')
-hold on
-scatter(1:N_participants, results_pitch(:,5), 100,'d')
+scatter(1:N_participants, results_pitch(:,4), results_pitch(:,8),'filled')
+alpha(0.8)
 hold on
 
 
+title("Pitch of speech")
 
 xlabel("Participant")
 ylabel("Pitch of speech")
